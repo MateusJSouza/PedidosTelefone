befb45fb210c662aece170c3bc0d93b8f98c15a2
MateusJesus <mateusjdev@gmail.com>
2021-01-26 14:45:01 -0300

    ✨

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <title>Registro de Pedidos 1.0</title>
+</head>
+<body>
+  <header>
+    <h2>Efetue seu login</h2>
+  </header>
+
+  <main class="container">
+
+    <section class="form-section">
+      <form id="form-login" action="login.php" method="POST">
+
+        <?php if (isset($resultado) && $resultado["cod"] == 0): ?>
+        <div class="alert alert-danger">
+          <?php 
+            echo $resultado["msg"];
+          ?>
+        </div>
+        <?php endif; ?>
+
+        <label for="email">E-mail</label><br>
+        <input type="email" id="email" name="email" placeholder="Digite seu email"><br><br>
+  
+        <label for="password">Senha</label><br>
+        <input type="password" id="senha" name="password" placeholder="Digite sua senha"><br><br>
+  
+        <input type="submit" id="submeter" value="Entrar" class="btn btn-primary">
+  
+      </form>
+    </section>
+  </main>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+  if (count($_POST) > 0) {
+    // 1. Pegar os valores do formulário
+    $email = $_POST['email'];
+    $senha = $_POST['password'];
+
+    // 2. Conexão com o banco de dados
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=restaurante_bd", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      // echo "Connected successfully";
+
+      $stmt = $conn->prepare("SELECT codigo FROM usuario WHERE email=:email AND senha=md5(:senha)");
+      $stmt ->bindParam(':email', $email, PDO::PARAM_STR);
+      $stmt ->bindParam(':senha', $senha, PDO::PARAM_STR);
+      $stmt->execute();
+    
+      // set the resulting array to associative
+      $result = $stmt->fetchAll();
+      $qtd_users = count($result);
+      if ($qtd_users == 1) {
+        // TODO substituir pelo redirecionamento.
+        $resultado["msg"] = "E-mail e senha conferem!";
+        $resultado["cod"] = 1;
+      } else if ($qtd_users == 0){
+        $resultado["msg"] = "E-mail e senha não conferem";
+        $resultado["cod"] = 0;
+      }
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+      }
+      $conn = null;
+
+    // 3. Verificar se e-mail e senha estão no BD
+  }
+  include("index.php");
+
+?>
